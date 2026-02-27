7decf87d39f61da29969af1a4369547d4bbc9a85
Calvin Yu <cyex@Calvins-MacBook-Pro.local>
2026-02-27 18:55:26 -0500

    Used "here" package for file sourcing

--- R/tests scripts/analyze_plant.R
+++ R/tests scripts/analyze_plant.R
@@ -7,7 +7,7 @@ cat("Current working directory:", getwd(), "\n")
 
 required_packages <- c(
   "dplyr", "tidyr", "ggplot2", "mgcv", "fields",
-  "purrr", "patchwork", "viridis", "scales", "knitr"
+  "purrr", "patchwork", "viridis", "scales", "knitr", "here"
 )
 
 for (pkg in required_packages) {
@@ -36,9 +36,10 @@ function_files <- c(
 )
 
 for (f in function_files) {
-  if (file.exists(f)) {
-    source(f)
-    cat("Sourced:", f, "\n")
+  function_path <- here("R",f)
+  if (file.exists(function_path)){
+    source(function_path)
+    cat("Sourced", f, "\n")
   } else {
     cat("File not found:", f, "\n")
   }
@@ -51,10 +52,10 @@ for (f in function_files) {
 cat("\n3. Data loading and exploration\n")
 
 data_files <- list(
-  data1 = "Aster_analyses_2011_Cohort.txt",
-  data2 = "Aster_analyses_2012_Cohort_full.txt",
-  data3 = "Aster_analyses_2011_Cohort_full.txt",
-  data4 = "Aster_analyses_2012_Cohort.txt"
+  data1 = here("R","test_data","Aster_analyses_2011_Cohort.txt"),
+  data2 = here("R","test_data","Aster_analyses_2012_Cohort_full.txt"),
+  data3 = here("R","test_data","Aster_analyses_2011_Cohort_full.txt"),
+  data4 = here("R","test_data","Aster_analyses_2012_Cohort.txt")
 )
 
 data1 <- read.delim(data_files$data1, sep = "\t")
